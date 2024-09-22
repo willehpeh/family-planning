8fba7ca3354a772f918e07c5de3a928f1c6bfed7
w.alexander <will_alexander@me.com>
2024-09-22 16:08:22 +0200

    Remove e2e, clean files

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,30 @@
-const nx = require("@nx/eslint-plugin");
+const nx = require('@nx/eslint-plugin');
 
 module.exports = [
-...nx.configs["flat/base"],
-...nx.configs["flat/typescript"],
-...nx.configs["flat/javascript"],
-{
-    files: [
-        "**/*.ts",
-        "**/*.tsx",
-        "**/*.js",
-        "**/*.jsx"
-    ],
-    rules: { "@nx/enforce-module-boundaries": [
-            "error",
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+          depConstraints: [
             {
-                enforceBuildableLibDependency: true,
-                allow: ["^.*/eslint(\\.base)?\\.config\\.[cm]?js$"],
-                depConstraints: [{
-                        sourceTag: "*",
-                        onlyDependOnLibsWithTags: ["*"]
-                    }]
-            }
-        ] }
-},
-{
-    files: [
-        "**/*.ts",
-        "**/*.tsx",
-        "**/*.js",
-        "**/*.jsx"
-    ],
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     // Override or add rules here
-    rules: {}
-},];
+    rules: {},
+  },
+];
