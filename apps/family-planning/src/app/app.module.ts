058c8e71e41473aaff3939be829ba7320b49db01
w.alexander <will_alexander@me.com>
2024-10-28 21:52:44 +0100

    Add NestJS config

--- apps/family-planning/src/app/app.module.ts
+++ apps/family-planning/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
