4f49ec7c24b84770cd53046e91e2ab68e873a5e3
w.alexander <will_alexander@me.com>
2024-10-29 22:22:54 +0100

    Add create todo list dto

--- libs/family-planning-application/src/lists/todo/create-todo-list/create-todo-list.command.ts
+++ libs/family-planning-application/src/lists/todo/create-todo-list/create-todo-list.command.ts
@@ -1,6 +1,8 @@
+import { CreateTodoListDto } from './create-todo-list.dto';
+
 export class CreateTodoListCommand {
   public readonly name: string;
-  constructor({ name }: { name: string }) {
+  constructor({ name }: CreateTodoListDto) {
     this.name = name;
   }
 }
