251501e1495b38079bddb00197194b8efb1f6c8b
w.alexander <will_alexander@me.com>
2024-11-02 23:36:13 +0100

    Add Postgres db, add TypeORM base config

--- apps/family-planning/src/app/app.module.ts
+++ apps/family-planning/src/app/app.module.ts
@@ -3,12 +3,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
-      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: true,
     })
   ],
   controllers: [AppController],
