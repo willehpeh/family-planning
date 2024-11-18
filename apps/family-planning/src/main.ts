fba3b73c5db560b87bcd0beaa387ec1e2c7d606a
w.alexander <will_alexander@me.com>
2024-11-18 22:43:29 +0100

    App uses session and SQLiteStore

--- apps/family-planning/src/main.ts
+++ apps/family-planning/src/main.ts
@@ -1,10 +1,7 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import session from 'express-session';
+import connectSQlite from 'connect-sqlite3';
 
 import { AppModule } from './app/app.module';
 
@@ -13,6 +10,14 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
+  const SQLiteStore = connectSQlite(session);
+  app.use(session({
+    store: new SQLiteStore,
+    secret: 'your secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true },
+  }))
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
