707a7961ce8c638e91baf31cdcee5cc348077a44
w.alexander <will_alexander@me.com>
2024-11-20 22:42:21 +0100

    Add artificial wait

--- apps/family-planning/src/app/auth/auth.module.ts
+++ apps/family-planning/src/app/auth/auth.module.ts
@@ -4,13 +4,17 @@ import { AuthController } from './controllers/auth.controller';
 import { ConfigService } from '@nestjs/config';
 import { Issuer } from 'openid-client';
 import { AuthService } from './services/auth.service';
+import { delay, lastValueFrom, of } from 'rxjs';
 
 @Module({
   imports: [PassportModule],
   controllers: [AuthController],
   providers: [
     { provide: 'KEYCLOAK_ISSUER',
-      useFactory: async (configService: ConfigService) => await Issuer.discover(configService.get('KEYCLOAK_ISSUER')),
+      useFactory: async (configService: ConfigService) => {
+        await lastValueFrom(of(true).pipe(delay(5000)));
+        return await Issuer.discover(configService.get('KEYCLOAK_ISSUER'));
+      },
       inject: [ConfigService]
     },
     AuthService,
