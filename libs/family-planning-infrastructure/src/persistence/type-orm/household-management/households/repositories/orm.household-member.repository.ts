0fb0d689314d23f25a239f3bb65059c009a3b836
w.alexander <will_alexander@me.com>
2024-12-01 00:18:55 +0100

    Set up ORM repositories for household ID

--- libs/family-planning-infrastructure/src/persistence/type-orm/household-management/households/repositories/orm.household-member.repository.ts
+++ libs/family-planning-infrastructure/src/persistence/type-orm/household-management/households/repositories/orm.household-member.repository.ts
@@ -1,4 +1,4 @@
-import { HouseholdMember, HouseholdMemberRepository } from '@family-planning/domain';
+import { HouseholdMember, HouseholdMemberReadModel, HouseholdMemberRepository } from '@family-planning/domain';
 import { HouseholdMember as HouseholdMemberEntity } from '../entities/household-member.entity';
 import { Repository } from 'typeorm';
 import { HouseholdMemberMapper } from '../mappers/household-member.mapper';
@@ -12,4 +12,7 @@ export class OrmHouseholdMemberRepository implements HouseholdMemberRepository {
     await this.householdMemberRepository.save(entity);
   }
 
+  async findByUserId(userId: string): Promise<HouseholdMemberReadModel> {
+    return this.householdMemberRepository.findOneOrFail({ where: { userId } });
+  }
 }
--- libs/family-planning-infrastructure/src/persistence/type-orm/household-management/households/repositories/orm.household.repository.ts
+++ libs/family-planning-infrastructure/src/persistence/type-orm/household-management/households/repositories/orm.household.repository.ts
@@ -1,4 +1,4 @@
-import { Household, HouseholdRepository } from '@family-planning/domain';
+import { Household, HouseholdReadModel, HouseholdRepository } from '@family-planning/domain';
 import { Household as HouseholdEntity } from '../entities/household.entity';
 import { Repository } from 'typeorm';
 import { HouseholdMapper } from '../mappers/household.mapper';
@@ -11,5 +11,14 @@ export class OrmHouseholdRepository implements HouseholdRepository {
     const entity = HouseholdMapper.toPersistence(household);
     await this.householdRepository.save(entity);
   }
+
+  async findByMemberId(id: string): Promise<HouseholdReadModel> {
+    return await this.householdRepository
+      .createQueryBuilder('household')
+      .where('household.memberIds @> :memberId', { memberId: JSON.stringify([id]) })
+      .getOneOrFail();
+  }
+
+
 
 }
