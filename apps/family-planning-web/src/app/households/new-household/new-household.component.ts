fa73fe430c75558f05d6297f02f894c209c29311
w.alexander <will_alexander@me.com>
2024-12-04 23:40:21 +0100

    Style new household page

--- apps/family-planning-web/src/app/households/new-household/new-household.component.ts
+++ apps/family-planning-web/src/app/households/new-household/new-household.component.ts
@@ -1,11 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, inject, Signal } from '@angular/core';
 import { CommonModule } from "@angular/common";
+import { Store } from '@ngrx/store';
+import { selectUserGivenName } from '../../auth/state/auth.selectors';
+import { ButtonComponent } from '../../ui-elements/button/button.component';
+import { CardComponent } from '../../ui-elements/card/card.component';
 
 @Component({
   selector: "app-new-household",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent, CardComponent],
   templateUrl: "./new-household.component.html",
   styleUrl: "./new-household.component.scss",
 })
-export class NewHouseholdComponent {}
+export class NewHouseholdComponent {
+  private store = inject(Store);
+  userGivenName: Signal<string>;
+
+  constructor() {
+    this.userGivenName = this.store.selectSignal(selectUserGivenName);
+  }
+}
