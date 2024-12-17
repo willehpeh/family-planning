04c77398922dc5719e7129acffa578837aaeee61
w.alexander <will_alexander@me.com>
2024-12-17 22:58:33 +0100

    Revert "Add event emitter to fake event bus"
    This reverts commit 31043afbce010b62ab651a1df9230c5cda11124c.

--- libs/family-planning-application/src/household-management/households/test-fixtures/fake.event-bus.ts
+++ libs/family-planning-application/src/household-management/households/test-fixtures/fake.event-bus.ts
@@ -1,23 +1,11 @@
 import { DomainEvent, EventBus } from '@family-planning/domain';
-import { Observable, Subject } from 'rxjs';
 
 export class FakeEventBus implements EventBus {
 
   events: DomainEvent[] = [];
-  events$: Observable<DomainEvent>;
-
-  private _events$: Subject<DomainEvent> = new Subject<DomainEvent>();
-
-  constructor() {
-    this.events$ = this._events$.asObservable();
-  }
 
   publish(event: DomainEvent): Promise<void> {
     this.events.push(event);
     return Promise.resolve();
-  }
-
-  emitEvent(event: DomainEvent): void {
-    this._events$.next(event);
   }
 }
