0e998bd41239bf3982c94c4e9d31ebf8e0cc9ce5
w.alexander <will_alexander@me.com>
2024-12-18 22:39:41 +0100

    Remove unnecessary abstraction, convert event-bus to abstract class

--- libs/family-planning-domain/src/common/contracts/event-bus.ts
+++ libs/family-planning-domain/src/common/contracts/event-bus.ts
@@ -1,5 +1,5 @@
 import { DomainEvent } from './domain-event';
 
-export interface EventBus {
-  publish(event: DomainEvent): Promise<void>;
+export abstract class EventBus {
+  abstract publish(event: DomainEvent): Promise<void>;
 }
