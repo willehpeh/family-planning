5396824584fa87371e9c63e66c458fc00da8bf48
w.alexander <will_alexander@me.com>
2024-12-22 23:04:44 +0100

    Allow logging out

--- apps/family-planning-web/src/app/layout/side-menu/side-menu.component.ts
+++ apps/family-planning-web/src/app/layout/side-menu/side-menu.component.ts
@@ -6,6 +6,7 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { Store } from '@ngrx/store';
 import { CloseSideMenu } from '../state/ui/ui.actions';
 import { SideMenuItemComponent } from './side-menu-item/side-menu-item.component';
+import { Logout } from '../../auth/state/auth.actions';
 
 @Component({
   selector: 'app-side-menu',
@@ -19,12 +20,17 @@ export class SideMenuComponent {
   protected readonly faArrowRightFromBracket = faArrowRightFromBracket;
   protected readonly faHouse = faHouse;
   protected readonly menuItems = [
-    { label: 'My Household', iconDefinition: faHouse },
-    { label: 'Logout', iconDefinition: faArrowRightFromBracket }
+    { label: 'My Household', iconDefinition: faHouse, action: () => {} },
+    { label: 'Logout', iconDefinition: faArrowRightFromBracket, action: () => this.onLogout() },
   ]
   private readonly store = inject(Store);
 
   onCloseMenu() {
     this.store.dispatch(CloseSideMenu());
+  }
+
+  onLogout() {
+    this.store.dispatch(CloseSideMenu());
+    this.store.dispatch(Logout());
   }
 }
