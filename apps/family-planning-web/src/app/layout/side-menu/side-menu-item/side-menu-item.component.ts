19db7e4ae78e6abe8c67e6d296bda243626a4b5b
w.alexander <will_alexander@me.com>
2024-12-22 23:01:02 +0100

    Refactor side menu

---
+++ apps/family-planning-web/src/app/layout/side-menu/side-menu-item/side-menu-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: "app-side-menu-item",
+  standalone: true,
+  imports: [CommonModule, FaIconComponent],
+  templateUrl: "./side-menu-item.component.html",
+  styleUrl: "./side-menu-item.component.scss",
+})
+export class SideMenuItemComponent {
+  label = input.required<string>();
+  iconDefinition = input<IconDefinition>();
+}
--- apps/family-planning-web/src/app/layout/side-menu/side-menu.component.ts
+++ apps/family-planning-web/src/app/layout/side-menu/side-menu.component.ts
@@ -1,26 +1,30 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../ui-elements/button/button.component';
 import { faArrowRightFromBracket, faHouse, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { Store } from '@ngrx/store';
 import { CloseSideMenu } from '../state/ui/ui.actions';
+import { SideMenuItemComponent } from './side-menu-item/side-menu-item.component';
 
 @Component({
-  selector: "app-side-menu",
+  selector: 'app-side-menu',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, FaIconComponent],
-  templateUrl: "./side-menu.component.html",
-  styleUrl: "./side-menu.component.scss",
+  imports: [CommonModule, ButtonComponent, FaIconComponent, SideMenuItemComponent],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.scss',
 })
 export class SideMenuComponent {
   protected readonly faXmark = faXmark;
+  protected readonly faArrowRightFromBracket = faArrowRightFromBracket;
+  protected readonly faHouse = faHouse;
+  protected readonly menuItems = [
+    { label: 'My Household', iconDefinition: faHouse },
+    { label: 'Logout', iconDefinition: faArrowRightFromBracket }
+  ]
   private readonly store = inject(Store);
 
   onCloseMenu() {
     this.store.dispatch(CloseSideMenu());
   }
-
-  protected readonly faArrowRightFromBracket = faArrowRightFromBracket;
-  protected readonly faHouse = faHouse;
 }
