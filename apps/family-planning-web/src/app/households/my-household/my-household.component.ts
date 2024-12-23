55eb412e00f6196ec92b1b8f8f342f8b1417eb67
w.alexander <will_alexander@me.com>
2024-12-23 22:46:15 +0100

    Basic template for my household

--- apps/family-planning-web/src/app/households/my-household/my-household.component.ts
+++ apps/family-planning-web/src/app/households/my-household/my-household.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ScaffoldingComponent } from '../../layout/scaffolding/scaffolding.component';
+import { CardComponent } from '../../ui-elements/card/card.component';
 
 @Component({
   selector: "app-my-household",
   standalone: true,
-  imports: [CommonModule, ScaffoldingComponent],
+  imports: [CommonModule, ScaffoldingComponent, CardComponent],
   templateUrl: "./my-household.component.html",
   styleUrl: "./my-household.component.scss",
 })
