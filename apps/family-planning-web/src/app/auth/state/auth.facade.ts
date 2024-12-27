221ea2e4a0f94294331bee5fa17fa34a988fd389
w.alexander <will_alexander@me.com>
2024-12-27 22:37:04 +0100

    Add UserInfo facade

---
+++ apps/family-planning-web/src/app/auth/state/auth.facade.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { filter, Observable } from 'rxjs';
+import { UserInfoDto } from '../types/user-info.dto';
+import { LoadUserInfo } from './auth.actions';
+import { selectUserInfo } from './auth.selectors';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthFacade {
+  private readonly store = inject(Store);
+
+  userInfo(): Observable<UserInfoDto> {
+    this.store.dispatch(LoadUserInfo());
+    return this.store.select(selectUserInfo).pipe(
+      filter(userInfo => !!userInfo)
+    );
+  }
+}
--- apps/family-planning-web/src/app/dashboard/resolvers/user-info.resolver.ts
+++ apps/family-planning-web/src/app/dashboard/resolvers/user-info.resolver.ts
@@ -1,16 +1,11 @@
 import { UserInfoDto } from '../../auth/types/user-info.dto';
 import { ResolveFn } from '@angular/router';
 import { inject } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { selectUserInfo } from '../../auth/state/auth.selectors';
-import { filter, take } from 'rxjs';
-import { LoadUserInfo } from '../../auth/state/auth.actions';
+import { take } from 'rxjs';
+import { AuthFacade } from '../../auth/state/auth.facade';
 
 export const userInfoResolver: ResolveFn<UserInfoDto> = () => {
-  const store = inject(Store);
-  store.dispatch(LoadUserInfo());
-  return store.select(selectUserInfo).pipe(
-    filter(info => !!info),
+  return inject(AuthFacade).userInfo().pipe(
     take(1)
-  );
+  )
 };
