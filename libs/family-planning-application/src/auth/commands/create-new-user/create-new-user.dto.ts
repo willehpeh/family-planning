a2b9643425020f8354940c73695266843d70dde6
w.alexander <will_alexander@me.com>
2024-12-30 23:14:39 +0100

    Refactor CreateNewUserDto to type alias instead of class

--- libs/family-planning-application/src/auth/commands/create-new-user/create-new-user.dto.ts
+++ libs/family-planning-application/src/auth/commands/create-new-user/create-new-user.dto.ts
@@ -1,16 +1,7 @@
-export class CreateNewUserDto {
-
+export type CreateNewUserDto = {
   readonly firstName: string;
   readonly lastName: string;
   readonly email: string;
   readonly householdId: string;
   readonly memberId: string;
-
-  constructor(details: { firstName: string, lastName: string, email: string, householdId: string, memberId: string }) {
-    this.firstName = details.firstName;
-    this.lastName = details.lastName;
-    this.email = details.email;
-    this.householdId = details.householdId;
-    this.memberId = details.memberId;
-  }
 }
