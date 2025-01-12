0baeb4a542b3219c4fffb459d62a2145e7b55b70
w.alexander <will_alexander@me.com>
2025-01-12 23:27:47 +0100

    Refactor and improve styling of todo lists list

--- apps/family-planning-web/src/app/lists/components/todo-list-grid-cell/todo-list-grid-cell.component.ts
+++ apps/family-planning-web/src/app/lists/components/todo-list-grid-cell/todo-list-grid-cell.component.ts
@@ -1,47 +1,41 @@
-import { Component, computed, HostBinding, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesome';
 import { faListCheck } from '@fortawesome/free-solid-svg-icons';
-import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list-grid-cell',
   standalone: true,
   imports: [
     FaIconComponent,
-    NgClass
   ],
   template: `
-		<div class="bg-black bg-opacity-60 
-		              rounded-lg border-2 
-		              text-center box-border
-		              cursor-pointer p-3
-		              w-full h-full
-		              hover:bg-gray-900
-		              active:bg-gray-800"
-				 [title]="listName()"
-         [ngClass]="classes()"
+		<div [title]="listName()"
+         [class]="classes()"
     >
-			<fa-icon [icon]="icon()" size="2x"/>
-			<h2 class="mt-2"
+			<fa-icon [icon]="icon()" size="2x" />
+			<h2
           [class.text-amber-300]="highlight()">
-        {{ shortenedListName() }}
+        {{ listName() }}
       </h2>
 		</div>
   `,
 })
 export class TodoListGridCellComponent {
   listName = input.required<string>();
-  highlight = input<boolean>();
+  highlight = input<boolean>(false);
   icon = input<IconDefinition>(faListCheck);
-  shortenedListName = computed(() => this.listName().length <= 10 ? this.listName() :`${this.listName().slice(0, 10)}...`);
+  index = input<number>(0);
 
   protected readonly classes = computed(() => {
-    return this.highlight() ?
+
+    const baseClasses = 'bg-black bg-opacity-60 border-2 rounded-lg flex gap-6 items-center box-border cursor-pointer p-3 w-full max-w-screen-md m-auto';
+
+    const borderColors = this.highlight() ?
       'border-amber-300 hover:border-amber-500 active:bg-amber-800' :
       'border-blue-300 hover:border-blue-500 active:bg-blue-800';
-  });
 
-  @HostBinding('class') get hostClasses() {
-    return 'w-1/3 max-w-36 aspect-square';
-  }
+    const topBorder = this.index() !== 0 ? 'border-t-0' : '';
+
+    return [baseClasses, borderColors, topBorder].join(' ');
+  });
 }
