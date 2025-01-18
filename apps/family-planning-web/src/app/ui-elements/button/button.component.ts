f4967f6ce96dc7e24e0f1a5c45a876b9ffc0e9b4
w.alexander <will_alexander@me.com>
2025-01-18 22:15:03 +0100

    Add "none" button style for toolbar icons

--- apps/family-planning-web/src/app/ui-elements/button/button.component.ts
+++ apps/family-planning-web/src/app/ui-elements/button/button.component.ts
@@ -19,9 +19,10 @@ export class ButtonComponent {
 
   disabled = input<boolean>(false);
   click = output<MouseEvent>();
-  buttonStyle = input<'primary' | 'outline'>('primary');
+  buttonStyle = input<'primary' | 'outline' | 'none'>('primary');
   primary = computed(() => this.buttonStyle() === 'primary');
   outline = computed(() => this.buttonStyle() === 'outline');
+  none = computed(() => this.buttonStyle() === 'none');
   classes = computed(() => ({
     'text-gray-900': this.primary(),
     'bg-amber-300': this.primary() && !this.disabled(),
@@ -29,12 +30,13 @@ export class ButtonComponent {
     'hover:bg-amber-200': this.primary() && !this.disabled(),
     'bg-amber-800': this.primary() && this.disabled(),
     'border-amber-800': this.primary() && this.disabled(),
-    'bg-transparent': this.outline(),
+    'bg-transparent': this.outline() || this.none(),
     'border-blue-300': this.outline() && !this.disabled(),
     'hover:bg-gray-900': this.outline() && !this.disabled(),
     'active:translate-x-0.5': !this.disabled(),
     'active:translate-y-0.5': !this.disabled(),
-    'italic': this.disabled()
+    'italic': this.disabled(),
+    'border-none': this.none(),
   }));
 
   onClick($event: MouseEvent) {
