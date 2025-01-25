618e65ba0af3425d8a74af3daf87d5e6048829d3
w.alexander <will_alexander@me.com>
2025-01-25 22:06:08 +0100

    Fully remove household member repositories

--- libs/family-planning-domain/src/household-management/households/repositories/household-member.repository.ts
+++
@@ -1,5 +0,0 @@
-import { HouseholdMemberReadModel } from '../read-models';
-
-export abstract class HouseholdMemberRepository {
-  abstract findByUserId(userId: string): Promise<HouseholdMemberReadModel | null>;
-}
--- libs/family-planning-domain/src/household-management/households/repositories/index.ts
+++ libs/family-planning-domain/src/household-management/households/repositories/index.ts
@@ -1,2 +1,1 @@
 export * from './household.repository';
-export * from './household-member.repository';
