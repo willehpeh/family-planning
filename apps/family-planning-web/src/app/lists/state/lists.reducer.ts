239f8b7382952dce0ec1cf7259fdace5157de3f6
w.alexander <will_alexander@me.com>
2025-02-07 22:35:23 +0100

    Clean lists reducer imports

--- apps/family-planning-web/src/app/lists/state/lists.reducer.ts
+++ apps/family-planning-web/src/app/lists/state/lists.reducer.ts
@@ -1,23 +1,7 @@
 import { createEntityAdapter, EntityState } from '@ngrx/entity';
 import { NullTodoListReadModel } from '../models/null-todo-list.read-model';
 import { createFeature, createReducer, createSelector, on } from '@ngrx/store';
-import {
-  CreateList,
-  CreateListFailure,
-  CreateListItem,
-  CreateListItemFailure,
-  CreateListItemSuccess,
-  CreateListSuccess,
-  LoadAllLists,
-  LoadAllListsFailure,
-  LoadAllListsFromDetailView,
-  LoadAllListsSuccess,
-  MarkDoneItemAsPending,
-  MarkDoneItemAsPendingFailure,
-  MarkItemAsDone,
-  MarkItemAsDoneFailure,
-  ToggleDisplayCompletedItems
-} from './lists.actions';
+import * as ListActions from './lists.actions';
 import { TodoListReadModel } from '@family-planning/domain';
 import { ListItemsAdapter } from './list-Items.adapter';
 
@@ -46,18 +30,18 @@ export const listsFeature = createFeature({
   name: featureKey,
   reducer: createReducer(
     initialState,
-    on(LoadAllLists, LoadAllListsFromDetailView, state => ({ ...state, loading: true })),
-    on(LoadAllListsSuccess, (state, { lists }) => adapter.upsertMany(lists, { ...state, loading: false })),
-    on(LoadAllListsFailure, state => ({ ...state, loading: false })),
-    on(CreateList, state => ({ ...state, saving: true })),
-    on(CreateListSuccess, (state, { list }) => adapter.addOne(list, { ...state, saving: false })),
-    on(CreateListFailure, state => ({ ...state, saving: false })),
-    on(CreateListItem, (state, { listId, temporaryItem }) => listItemsAdapter.addTemporaryItemToList(listId, state, temporaryItem)),
-    on(CreateListItemSuccess, (state, { listId, temporaryItemId, createdItem }) => listItemsAdapter.makeItemPermanent(listId, state, temporaryItemId, createdItem)),
-    on(CreateListItemFailure, (state, { listId, temporaryItemId }) => listItemsAdapter.revertListItems(listId, state, temporaryItemId)),
-    on(MarkItemAsDone, MarkDoneItemAsPendingFailure, (state, { listId, itemId }): ListsState => listItemsAdapter.markItemAsDone(listId, state, itemId)),
-    on(MarkItemAsDoneFailure, MarkDoneItemAsPending, (state, { listId, itemId }): ListsState => listItemsAdapter.markItemAsPending(listId, state, itemId)),
-    on(ToggleDisplayCompletedItems, state => ({ ...state, displayCompleted: !state.displayCompleted }))
+    on(ListActions.LoadAllLists, ListActions.LoadAllListsFromDetailView, state => ({ ...state, loading: true })),
+    on(ListActions.LoadAllListsSuccess, (state, { lists }) => adapter.upsertMany(lists, { ...state, loading: false })),
+    on(ListActions.LoadAllListsFailure, state => ({ ...state, loading: false })),
+    on(ListActions.CreateList, state => ({ ...state, saving: true })),
+    on(ListActions.CreateListSuccess, (state, { list }) => adapter.addOne(list, { ...state, saving: false })),
+    on(ListActions.CreateListFailure, state => ({ ...state, saving: false })),
+    on(ListActions.CreateListItem, (state, { listId, temporaryItem }) => listItemsAdapter.addTemporaryItemToList(listId, state, temporaryItem)),
+    on(ListActions.CreateListItemSuccess, (state, { listId, temporaryItemId, createdItem }) => listItemsAdapter.makeItemPermanent(listId, state, temporaryItemId, createdItem)),
+    on(ListActions.CreateListItemFailure, (state, { listId, temporaryItemId }) => listItemsAdapter.revertListItems(listId, state, temporaryItemId)),
+    on(ListActions.MarkItemAsDone, ListActions.MarkDoneItemAsPendingFailure, (state, { listId, itemId }): ListsState => listItemsAdapter.markItemAsDone(listId, state, itemId)),
+    on(ListActions.MarkItemAsDoneFailure, ListActions.MarkDoneItemAsPending, (state, { listId, itemId }): ListsState => listItemsAdapter.markItemAsPending(listId, state, itemId)),
+    on(ListActions.ToggleDisplayCompletedItems, state => ({ ...state, displayCompleted: !state.displayCompleted }))
   ),
   extraSelectors: ({ selectIds, selectEntities }) => ({
     selectAllLists: createSelector(
